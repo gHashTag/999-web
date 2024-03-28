3b21100e8d134c8d320fc5de61fff528af2950b6
Dmitriy Vasilev <raoffonom@icloud.com>
2024-03-28 19:04:48 +0700

    fix for meet

--- src/pages/[workspaceSlug]/create-meet/audio-spaces/[roomId]/index.tsx
+++ src/pages/[workspaceSlug]/create-meet/audio-spaces/[roomId]/index.tsx
@@ -1,68 +1,73 @@
 import { useState, useEffect } from "react";
 import Layout from "@/components/layout";
-// import {
-//   selectIsConnectedToRoom,
-//   useHMSActions,
-//   useHMSStore,
-// } from "@100mslive/react-sdk";
-// import { HMSPrebuilt } from "@100mslive/roomkit-react";
+import {
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from "@100mslive/react-sdk";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 
 import { useSupabase } from "@/hooks/useSupabase";
 
-// const Kanban = dynamic(() => import("@/components/Kanban/KanbanBoard"), {
-//   ssr: false,
-// });
+const HMSPrebuilt = dynamic(
+  () =>
+    import("@100mslive/roomkit-react").then((mod) => ({
+      default: mod.HMSPrebuilt,
+    })),
+  {
+    ssr: false,
+  }
+);
 
 const Rooms = () => {
   const router = useRouter();
-  // const { roomId } = router.query as { roomId: string };
-  // const { userSupabase } = useSupabase();
-  // const [token, setToken] = useState<string | undefined>(undefined);
-  // const isConnected = useHMSStore(selectIsConnectedToRoom);
-  // const hmsActions = useHMSActions();
+  const { roomId } = router.query as { roomId: string };
+  const { userSupabase } = useSupabase();
+  const [token, setToken] = useState<string | undefined>(undefined);
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
 
-  // useEffect(() => {
-  //   const fetchToken = async () => {
-  //     try {
-  //       if (typeof roomId === "string") {
-  //         const authToken = await hmsActions.getAuthTokenByRoomCode({
-  //           roomCode: roomId,
-  //         });
-  //         setToken(authToken);
-  //       } else {
-  //         throw new Error("roomCode is not a string");
-  //       }
-  //     } catch (error) {
-  //       // console.error("Ошибка при получении токена: ", error);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        if (typeof roomId === "string") {
+          const authToken = await hmsActions.getAuthTokenByRoomCode({
+            roomCode: roomId,
+          });
+          setToken(authToken);
+        } else {
+          throw new Error("roomCode is not a string");
+        }
+      } catch (error) {
+        // console.error("Ошибка при получении токена: ", error);
+      }
+    };
 
-  //   fetchToken();
-  // }, [hmsActions, roomId]);
+    fetchToken();
+  }, [hmsActions, roomId]);
 
-  // useEffect(() => {
-  //   const handleUnload = async () => {
-  //     if (isConnected) {
-  //       try {
-  //         await hmsActions.leave();
-  //       } catch (error) {
-  //         // console.error("Ошибка при попытке покинуть комнату: ", error);
-  //       }
-  //     }
-  //   };
+  useEffect(() => {
+    const handleUnload = async () => {
+      if (isConnected) {
+        try {
+          await hmsActions.leave();
+        } catch (error) {
+          // console.error("Ошибка при попытке покинуть комнату: ", error);
+        }
+      }
+    };
 
-  //   window.addEventListener("beforeunload", handleUnload);
+    window.addEventListener("beforeunload", handleUnload);
 
-  //   return () => {
-  //     window.removeEventListener("beforeunload", handleUnload);
-  //   };
-  // }, [hmsActions, isConnected]);
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+  }, [hmsActions, isConnected]);
 
   return (
     <Layout>
-      <span>Hello</span>
-      {/* {token && (
+      {token && (
         <HMSPrebuilt
           authToken={token}
           roomCode={roomId}
@@ -72,7 +77,7 @@ const Rooms = () => {
               "",
           }}
         />
-      )} */}
+      )}
     </Layout>
   );
 };
