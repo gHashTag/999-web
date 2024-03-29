f76d89a6eb7b33201e11143b43b42180156cf08b
Dmitriy Vasilev <raoffonom@icloud.com>
2024-03-29 22:16:25 +0700

    create room

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -4,4 +4,5 @@ export const headers = {
   "Access-Control-Expose-Headers": "Content-Length, X-JSON",
   "Access-Control-Allow-Headers":
     "apikey,X-Client-Info, Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
+  "Access-Control-Allow-Credentials": "true",
 };
