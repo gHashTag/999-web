1752c91eb324c0db72327e872d5f688b27bc3cb5
Dmitriy Vasilev <raoffonom@icloud.com>
2024-03-29 18:43:44 +0700

    cors

--- src/pages/api/corsHeaders.ts
+++ src/pages/api/corsHeaders.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://dao999nft.com",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
