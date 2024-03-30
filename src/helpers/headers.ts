8e35861a04dd822f735928b678949f05d6dce20c
Dmitriy Vasilev <raoffonom@icloud.com>
2024-03-30 13:21:42 +0700

    fix for token6

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -2,10 +2,10 @@ export const headers = {
   "Content-Type": "application/json",
 };
 
-// export const corsHeaders = {
-//   "Access-Control-Allow-Origin": "https://dao999nft.com",
-//   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
-// };
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+};
 
 if (!process.env.NEXT_PUBLIC_100MS) {
   throw new Error("NEXT_PUBLIC_100MS is not set");
