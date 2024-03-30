756c09f2d9749a78300eaf0fd4e2ccacb5bc42d0
Dmitriy Vasilev <raoffonom@icloud.com>
2024-03-30 13:20:47 +0700

    fix for token5

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -2,16 +2,11 @@ export const headers = {
   "Content-Type": "application/json",
 };
 
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://dao999nft.com",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
-};
+// export const corsHeaders = {
+//   "Access-Control-Allow-Origin": "https://dao999nft.com",
+//   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
+// };
 
 if (!process.env.NEXT_PUBLIC_100MS) {
   throw new Error("NEXT_PUBLIC_100MS is not set");
 }
-
-export const myHeaders = {
-  Authorization: `Bearer ${process.env.NEXT_PUBLIC_100MS}`,
-  "Content-Type": "application/json",
-};
--- src/pages/api/corsHeaders.ts
+++ src/pages/api/corsHeaders.ts
@@ -1,6 +1,3 @@
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://dao999nft.com",
-  "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Origin": "*",
 };
