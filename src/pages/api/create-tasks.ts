e1472036420926452aa6f3b05d01148621fc1bfc
Dmitriy Vasilev <raoffonom@icloud.com>
2024-04-03 15:06:58 +0700

    fix for create tasks

--- src/pages/api/create-tasks.ts
+++ src/pages/api/create-tasks.ts
@@ -104,99 +104,100 @@ export default async function handler(
 
   try {
     const { type, data } = await req.body;
+    console.log(type, "type");
     console.log(data, "data");
     if (!data.room_id) {
       return res.status(200).json({
         message: `check init data ${JSON.stringify(data)}`,
       });
-    }
-    if (type === "transcription.success") {
-      const { data: roomAssetValidation } = await supabase
-        .from("room_assets")
-        .select("*")
-        .eq("room_id", data.room_id);
+    } else {
+      if (type === "transcription.success") {
+        const { data: roomAssetValidation } = await supabase
+          .from("room_assets")
+          .select("*")
+          .eq("room_id", data.room_id);
 
-      if (
-        roomAssetValidation &&
-        roomAssetValidation[0].room_id === data.room_id
-      ) {
-        return res.status(200).json({
-          message: "room_id is equal to data.data.room_id",
-        });
-      }
-      // Получаем прямую ссылку на текстовый файл транскрипции
-      const transcriptTextPresignedUrl = data.transcript_txt_presigned_url;
+        if (
+          roomAssetValidation &&
+          roomAssetValidation[0].room_id === data.room_id
+        ) {
+          return res.status(200).json({
+            message: "room_id is equal to data.data.room_id",
+          });
+        }
+        // Получаем прямую ссылку на текстовый файл транскрипции
+        const transcriptTextPresignedUrl = data.transcript_txt_presigned_url;
 
-      // Выполняем запрос к URL для получения текста транскрипции
-      const transcriptResponse = await fetch(transcriptTextPresignedUrl);
+        // Выполняем запрос к URL для получения текста транскрипции
+        const transcriptResponse = await fetch(transcriptTextPresignedUrl);
 
-      const transcription = await transcriptResponse.text();
+        const transcription = await transcriptResponse.text();
 
-      const summaryJsonPresignedUrl = data.summary_json_presigned_url;
+        const summaryJsonPresignedUrl = data.summary_json_presigned_url;
 
-      const summaryJSONResponse = await fetch(summaryJsonPresignedUrl);
-      if (!summaryJSONResponse.ok) {
-        throw new Error("Не удалось получить transcriptJSONResponse");
-      }
-      const summaryResponse = await summaryJSONResponse.json();
-      // console.log(summaryResponse, "summaryResponse");
+        const summaryJSONResponse = await fetch(summaryJsonPresignedUrl);
+        if (!summaryJSONResponse.ok) {
+          throw new Error("Не удалось получить transcriptJSONResponse");
+        }
+        const summaryResponse = await summaryJSONResponse.json();
+        // console.log(summaryResponse, "summaryResponse");
 
-      const summarySection = summaryResponse.sections.find(
-        (section: {
-          title: string;
-          format: string;
-          bullets: string[];
-          paragraph: string;
-        }) => section.title === "Short Summary"
-      );
-      // console.log(summarySection, "summarySection");
-      const summary_short = summarySection ? summarySection.paragraph : "";
-
-      // console.log(summary_short, "summary_short");
-
-      const getTitleWithEmojiSystemPrompt = `create a very short title with an emoji at the beginning of this text`;
-
-      const titleWithEmoji = await createChatCompletion(
-        summary_short,
-        getTitleWithEmojiSystemPrompt
-      );
-      // console.log(titleWithEmoji, "titleWithEmoji");
-
-      const roomAsset = {
-        ...data,
-        title: titleWithEmoji,
-        summary_short,
-        transcription,
-      };
-      // console.log("roomAsset", roomAsset);
-
-      const { error: errorInsertRoomAsset } = await supabase
-        .from("room_assets")
-        .insert([roomAsset]);
-
-      if (errorInsertRoomAsset) {
-        throw new Error(
-          `Asset creation failed: ${errorInsertRoomAsset.message}`
+        const summarySection = summaryResponse.sections.find(
+          (section: {
+            title: string;
+            format: string;
+            bullets: string[];
+            paragraph: string;
+          }) => section.title === "Short Summary"
         );
-      }
+        // console.log(summarySection, "summarySection");
+        const summary_short = summarySection ? summarySection.paragraph : "";
 
-      const systemPrompt = `Answer with emoticons. You are an AI assistant working at dao999nft. Your goal is to extract all tasks from the text, the maximum number of tasks, the maximum number of tasks, the maximum number of tasks, the maximum number of tasks, the maximum number of tasks, assign them to executors using the colon sign: assignee, title,  description (Example: <b>Nikita Zhilin</b>: 💻 Develop functional requirements) Provide your response as a JSON object`;
+        // console.log(summary_short, "summary_short");
 
-      const preparedTasks = await createChatCompletionJson(
-        transcription,
-        systemPrompt
-      );
-      // console.log("preparedTasks", preparedTasks);
-      const { data: users } = await supabase.from("users").select("*");
+        const getTitleWithEmojiSystemPrompt = `create a very short title with an emoji at the beginning of this text`;
 
-      const preparedUsers = getPreparedUsers(users);
-      // console.log(preparedUsers, "preparedUsers");
+        const titleWithEmoji = await createChatCompletion(
+          summary_short,
+          getTitleWithEmojiSystemPrompt
+        );
+        // console.log(titleWithEmoji, "titleWithEmoji");
 
-      const prompt = `add the 'user_id' from of ${JSON.stringify(
-        preparedUsers
-      )} to the objects of the ${JSON.stringify(
-        preparedTasks
-      )} array. (Example: [{
+        const roomAsset = {
+          ...data,
+          title: titleWithEmoji,
+          summary_short,
+          transcription,
+        };
+        // console.log("roomAsset", roomAsset);
+
+        const { error: errorInsertRoomAsset } = await supabase
+          .from("room_assets")
+          .insert([roomAsset]);
+
+        if (errorInsertRoomAsset) {
+          throw new Error(
+            `Asset creation failed: ${errorInsertRoomAsset.message}`
+          );
+        }
+
+        const systemPrompt = `Answer with emoticons. You are an AI assistant working at dao999nft. Your goal is to extract all tasks from the text, the maximum number of tasks, the maximum number of tasks, the maximum number of tasks, the maximum number of tasks, the maximum number of tasks, assign them to executors using the colon sign: assignee, title,  description (Example: <b>Nikita Zhilin</b>: 💻 Develop functional requirements) Provide your response as a JSON object`;
+
+        const preparedTasks = await createChatCompletionJson(
+          transcription,
+          systemPrompt
+        );
+        // console.log("preparedTasks", preparedTasks);
+        const { data: users } = await supabase.from("users").select("*");
+
+        const preparedUsers = getPreparedUsers(users);
+        // console.log(preparedUsers, "preparedUsers");
+
+        const prompt = `add the 'user_id' from of ${JSON.stringify(
+          preparedUsers
+        )} to the objects of the ${JSON.stringify(
+          preparedTasks
+        )} array. (Example: [{
         assignee: {
           user_id: "1a1e4c75-830c-4fe8-a312-c901c8aa144b",
           first_name: "Andrey",
@@ -207,65 +208,66 @@ export default async function handler(
         description: "Capture the Universe and a couple of stars in the Aldebaran constellation"
       }]) Provide your response as a JSON object and always response on English`;
 
-      // console.log(preparedTasks, "preparedTasks");
-      const tasks = await createChatCompletionJson(prompt);
+        // console.log(preparedTasks, "preparedTasks");
+        const tasks = await createChatCompletionJson(prompt);
 
-      const tasksArray = tasks && JSON.parse(tasks).tasks;
-      // console.log(tasksArray, "tasksArray");
-      if (Array.isArray(tasksArray)) {
-        const newTasks = tasksArray.map((task: any) => {
-          // Если user_id отсутствует или пуст, присваиваем значение по умолчанию
-          if (!task.assignee.user_id) {
-            task.assignee.user_id = "d685d450-9759-4cd2-96cb-f1dc132d3078";
+        const tasksArray = tasks && JSON.parse(tasks).tasks;
+        // console.log(tasksArray, "tasksArray");
+        if (Array.isArray(tasksArray)) {
+          const newTasks = tasksArray.map((task: any) => {
+            // Если user_id отсутствует или пуст, присваиваем значение по умолчанию
+            if (!task.assignee.user_id) {
+              task.assignee.user_id = "d685d450-9759-4cd2-96cb-f1dc132d3078";
+            }
+            return task;
+          });
+          // console.log(newTasks, "newTasks");
+
+          const { data: roomData } = (await supabase
+            .from("rooms")
+            .select("*")
+            .eq("room_id", data.room_id)) as { data: Data[]; error: any };
+
+          const { lang, chat_id, token } = roomData[0];
+
+          if (chat_id) {
+            sendTasksToTelegram(
+              chat_id,
+              newTasks,
+              summary_short,
+              lang,
+              token
+            ).catch(console.error);
           }
-          return task;
-        });
-        // console.log(newTasks, "newTasks");
 
-        const { data: roomData } = (await supabase
-          .from("rooms")
-          .select("*")
-          .eq("room_id", data.room_id)) as { data: Data[]; error: any };
+          for (const task of newTasks) {
+            // Убедитесь, что userId существует и не равен null
+            const user_id = task?.assignee?.user_id;
+            console.log(data.room_id, "data.room_id");
+            const taskData = await supabase.from("tasks").insert([
+              {
+                user_id,
+                title: task.title,
+                description: task.description,
+                room_id: data.room_id,
+              },
+            ]);
+            console.log(taskData, "taskData");
 
-        const { lang, chat_id, token } = roomData[0];
-
-        if (chat_id) {
-          sendTasksToTelegram(
-            chat_id,
-            newTasks,
-            summary_short,
-            lang,
-            token
-          ).catch(console.error);
-        }
-
-        for (const task of newTasks) {
-          // Убедитесь, что userId существует и не равен null
-          const user_id = task?.assignee?.user_id;
-          console.log(data.room_id, "data.room_id");
-          const taskData = await supabase.from("tasks").insert([
-            {
-              user_id,
-              title: task.title,
-              description: task.description,
-              room_id: data.room_id,
-            },
-          ]);
-          console.log(taskData, "taskData");
-
-          if (taskData.error?.message)
-            console.log("Error:", taskData.error.message);
+            if (taskData.error?.message)
+              console.log("Error:", taskData.error.message);
+          }
+        } else {
+          console.error("Error: 'tasks' is not an array or is null");
+          return res.status(500).json({
+            message: "Error: 'tasks' is not an array or is null",
+          });
         }
       } else {
-        console.error("Error: 'tasks' is not an array or is null");
-        return res.status(500).json({
-          message: "Error: 'tasks' is not an array or is null",
+        return res.status(200).json({
+          message: "type is not equal to transcription.success",
         });
       }
-    } else {
-      return res.status(200).json({
-        message: "type is not equal to transcription.success",
-      });
     }
   } catch (error: any) {
     console.log("error", error);
